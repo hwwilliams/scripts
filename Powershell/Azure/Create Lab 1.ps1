e816e4e82b0d1f36d7ec555df8b25154cd56ed97
saltystew <saltystew94@gmail.com>
2018-09-24 16:54:04 -0400

    initial commit

---
+++ Powershell/Azure/Create Lab 1.ps1
@@ -0,0 +1,27 @@
+﻿$Connection = Get-AutomationConnection -Name AzureRunAsConnection
+Connect-AzureRmAccount -ServicePrincipal -Tenant $Connection.TenantID -ApplicationId $Connection.ApplicationID -CertificateThumbprint $Connection.CertificateThumbprint
+
+Import-Module .\Modules\New-VirtualMachine
+
+$LocationName = "East US"
+$NetworkName = "Lab_1_vNetwork"
+$ResourceGroupName = "Lab01"
+$VMSize = "Standard_B2s"
+
+if (-not (Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue)) {
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $LocationName
+}
+
+if (-not (Get-AzureRmNetworkInterface -Name $NetworkName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
+    $NICName = "vNIC"
+    $SubnetName = "vSubnet"
+    $SubnetAddressPrefix = "10.0.0.0/24"
+    $vNetAddressPrefix = "10.0.0.0/16"
+    $SingleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
+    $vNet = New-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $vNetAddressPrefix -Subnet $SingleSubnet
+    $NIC = New-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $vNet.Subnets[0].Id
+}
+
+foreach ($VM in 1..3) {
+    New-VirtualMachine -AdminUser Hunter -AdminPassword Pa11word -LocationName $LocationName -ResourceGroupName $ResourceGroupName -VMName "Test0$VM" -OSType Windows
+}
---
+++ Powershell/Azure/Modules/New-VirtualMachine/New-VirtualMachine.psm1
@@ -0,0 +1,53 @@
+﻿Function Set-LinuxServerPublisher() {
+    $Publisher = "Canonical"
+    $PublisherOffer = "UbuntuServer"
+    $PublisherSkus = "16.04-LTS"
+}
+
+Function Set-WindowsDesktopPublisher() {
+    $Publisher = "MicrosoftWindowsDesktop"
+    $PublisherOffer = "Windows-10"
+    $PublisherSkus = "rs4-pror"
+}
+
+Function Set-WindowsServerPublisher() {
+    $Publisher = "MicrosoftWindowsServer"
+    $PublisherOffer = "WindowsServer"
+    $PublisherSkus = "2016-Datacenter"
+}
+
+Function New-VirtualMachine() {
+    Param(
+    $AdminUser,
+    $AdminPassword,
+    [Switch] $Desktop,
+    $LocationName,
+    $ResourceGroupName,
+    $VMName,
+    [ValidateSet('Linux','Windows')]
+    $OSType
+    )
+
+    $AdminUserPassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential ($AdminUser, $AdminUserPassword)
+
+    $VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+
+    if ($OSType -eq "Linux") {
+        $VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Linux -ComputerName $VMName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+        Set-LinuxPublisher
+    } elseif ($OSType -eq "Windows") {
+        $VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VMName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+        if ($Desktop) {
+            Set-WindowsDesktopPublisher
+        } else {
+            Set-WindowsServerPublisher
+        }
+    }
+
+    $VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+    $VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName $Publisher -Offer $PublisherOffer -Skus $PublisherSkus -Version latest
+    New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
+}
+
+Export-ModuleMember -Function 'New-VirtualMachine'
