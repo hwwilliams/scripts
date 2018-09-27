a91cfa4ad770d88454d729a0343e3e451ea5ce87
saltystew <saltystew94@gmail.com>
2018-09-27 11:34:22 -0400

    move files

--- Powershell/Azure/Create Lab 1.ps1
+++ Powershell/Azure/Create Lab 1.ps1
@@ -1,4 +1,4 @@
-﻿Remove-Variable * -ErrorAction SilentlyContinue
+Remove-Variable * -ErrorAction SilentlyContinue
 Remove-Module * -ErrorAction SilentlyContinue
 $error.Clear()
 
@@ -8,7 +8,7 @@ Connect-AzureRmAccount -ServicePrincipal -Tenant $Connection.TenantID -Applicati
 Import-Module .\Modules\New-AzureVirtualMachine
 
 $LocationName = "East US"
-$NetworkName = ""
+$NetworkName = "vNetwork"
 $ResourceGroupName = "Lab01"
 $VMSize = "Standard_B2s"
 
@@ -16,16 +16,19 @@ if (-not (Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction Silentl
     New-AzureRmResourceGroup -Name $ResourceGroupName -Location $LocationName
 }
 
-if (-not (Get-AzureRmNetworkInterface -Name $NetworkName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
+if (Get-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue) {
+    $vNet = (Get-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName)
+}
+elseif (-not (Get-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
     $NICName = "vNIC"
     $SubnetName = "vSubnet"
     $SubnetAddressPrefix = "10.0.0.0/24"
     $vNetAddressPrefix = "10.0.0.0/16"
     $SingleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
     $vNet = New-AzureRmVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $vNetAddressPrefix -Subnet $SingleSubnet
-    $NIC = New-AzureRmNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $vNet.Subnets[0].Id
 }
 
-Add-AzureRmVMNetworkInterface -VM $($VirtualMachine) -Id $NIC.Id
-
-New-AzureVirtualMachine -AdminUser hwadmin -AdminPassword '!{@zure}@Labs01!' -LocationName $LocationName -ResourceGroupName $ResourceGroupName -VMName Web01 -OSType ubuntu
+foreach ($VM in 1..1) {
+    $NIC = New-AzureRmNetworkInterface -Name "$NICName0$VM" -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $vNet.Subnets[0].Id
+    New-AzureVirtualMachine -AdminUser Hunter -AdminPassword Pa11word -LocationName $LocationName -ResourceGroupName $ResourceGroupName -VMName "Test0$VM" -OSType Windows -NetworkInterfaceID $NIC.Id
+}
