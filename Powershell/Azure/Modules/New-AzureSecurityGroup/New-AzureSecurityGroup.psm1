48a8800cc1e95e3edf0c5f7f387587608ee15217
saltystew <saltystew94@gmail.com>
2018-09-28 16:13:59 -0400

    initial commit

--- Powershell/Azure/Modules/New-AzureSecurityGroup/New-AzureSecurityGroup.psm1
+++ Powershell/Azure/Modules/New-AzureSecurityGroup/New-AzureSecurityGroup.psm1
@@ -1,1 +1,16 @@
-﻿
+﻿Function New-AzureSecurityGroup {
+    Param (
+
+    )
+    if (-not (Get-AzureRmNetworkSecurityGroup -Name $NSGName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue)) {
+        $AllowRDP = New-AzureRmNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound `
+            -Priority 100 -SourceAddressPrefix '96.66.217.173' -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
+        $AllowHTTP = New-AzureRmNetworkSecurityRuleConfig -Name web-rule -Description "Allow HTTP" -Access Allow -Protocol Tcp -Direction Inbound `
+            -Priority 101 -SourceAddressPrefix '96.66.217.173' -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+        $NetworkSecurityGroup = New-AzureRmNetworkSecurityGroup -Name $NSGName -ResourceGroupName $ResourceGroupName -Location $LocationName -SecurityRules $AllowRDP, $AllowHTTP
+    }
+    if (-not ($NIC.NetworkSecurityGroup.Id)) {
+        $NIC.NetworkSecurityGroup.Id = $NetworkSecurityGroup.Id
+        $NIC | Set-AzureRmNetworkInterface
+    }
+}
